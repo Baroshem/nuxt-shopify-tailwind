f443313fd04e0392a10c18dfa6aee52edf4adec5
Baroshem <jakub.andrzejewski.dev@gmail.com>
2022-06-03 16:17:59 +0200

    feat: ssr false

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@ import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  ssr: false,
   modules: ["@nuxtjs/tailwindcss", "nuxt-graphql-client"],
 
   runtimeConfig: {
