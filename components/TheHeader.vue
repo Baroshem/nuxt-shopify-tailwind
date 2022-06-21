294729ad6a0d2871522bf68750a7d75128c2eb34
Baroshem <jakub.andrzejewski.dev@gmail.com>
2022-06-21 14:31:37 +0200

    feat: add Algolia lint

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,3 +1,13 @@
+<script lang="ts" setup>
+const { result, search } = useSearch("headless_commerce");
+const hits = ref([]);
+
+const fetchSearchResults = async (e) => {
+  await search({ query: e.target.value });
+  hits.value = result.value.hits;
+};
+</script>
+
 <template>
   <nav class="py-2 relative w-full">
     <div class="px-6 flex items-center">
@@ -6,21 +16,29 @@
       </NuxtLink>
       <div class="flex justify-center relative">
         <div class="mb-3 w-96">
-          <input type="search" class="
-                block
-                w-full
-                px-3
-                py-1
-                bg-white bg-clip-padding
-                border border-solid border-gray-300
-                rounded
-              " id="search" placeholder="Search for products" @input="fetchSearchResults" />
+          <input
+            type="search"
+            class="block w-full px-3 py-1 bg-white bg-clip-padding border border-solid border-gray-300 rounded"
+            id="search"
+            placeholder="Search for products"
+            @input="fetchSearchResults"
+          />
         </div>
-        <div v-if="hits.length" class="flex justify-center absolute top-10 z-10">
+        <div
+          v-if="hits.length"
+          class="flex justify-center absolute top-10 z-10"
+        >
           <ul class="bg-white border border-gray-200 w-96 text-gray-900">
-            <li v-for="hit in result.hits" :key="hit.objectID" class="px-6 py-2 border-b border-gray-200 w-full"
-              @click="hits = []">
-              <NuxtLink :to="`/products/${hit.Handle}`" class="flex items-center">
+            <li
+              v-for="hit in hits"
+              :key="hit.objectID"
+              class="px-6 py-2 border-b border-gray-200 w-full"
+              @click="hits = []"
+            >
+              <NuxtLink
+                :to="`/products/${hit.Handle}`"
+                class="flex items-center"
+              >
                 <img class="w-20" :src="hit['Image Src']" />
                 <p class="ml-4">{{ hit.Title }}</p>
               </NuxtLink>
@@ -31,14 +49,3 @@
     </div>
   </nav>
 </template>
-
-<script lang="ts" setup>
-const { result, search } = useSearch('headless_commerce')
-const hits = ref([])
-
-const fetchSearchResults = async (e) => {
-  await search({ query: e.target.value })
-  hits.value = result.value.hits
-}
-</script>
-
